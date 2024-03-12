ae823a47c12545036c760012b93acd9bdf4a35be
PabloAriasTorres <114682557+PabloAriasTorres@users.noreply.github.com>
2024-03-12 18:40:07 +0100

    Hacer npm i

--- Mock Up/app.js
+++ Mock Up/app.js
@@ -16,6 +16,12 @@ app.get('/productos', (req, res) =>{
     res.send(file);
 });
 
+app.get('/usuarios', (req, res) =>{
+    res.setHeader('Content-type','text/json');
+    const file = fs.readFileSync('./usuarios.json','UTF-8');
+    res.send(file);
+});
+
 app.post('/buscador', (req, res) =>{
     let cadena = req.body.texto;
     const file = fs.readFileSync('./productos.json','UTF-8');
@@ -30,6 +36,84 @@ app.post('/buscador', (req, res) =>{
                 console.log("Archivo de resultados creado exitosamente");
                 const buscador = fs.readFileSync('./buscador.json','UTF-8');
                 res.send(buscador);
+            }
+        });
+    } catch (err) {
+        console.log("Error al analizar el archivo JSON:", err);
+    }
+});
+
+app.get('/favoritos', (req, res) =>{
+    const file = fs.readFileSync('./productos.json','UTF-8');
+    try {
+        const datos = JSON.parse(file);
+        const objetosFiltrados = datos.filter(p => p.favorito == "true");
+        fs.writeFile('./favoritos.json', JSON.stringify(objetosFiltrados), 'utf8', err => {
+            if (err) {
+                console.log("Error al escribir el archivo:", err);
+            }else{
+                console.log("Archivo de resultados creado exitosamente");
+                const favoritos = fs.readFileSync('./favoritos.json','UTF-8');
+                res.send(favoritos);
+            }
+        });
+    } catch (err) {
+        console.log("Error al analizar el archivo JSON:", err);
+    }
+});
+
+app.get('/recomendados', (req, res) =>{
+    const file = fs.readFileSync('./favoritos.json','UTF-8');
+    try {
+        const datos = JSON.parse(file);
+        var numInformatica = 0;
+        var numModa = 2;
+        var numHogar = 0;
+        var numLibros = 0;
+        datos.filter(p => {
+            if(p.categoria["nombre"] == "Moda") numModa++;
+            else if(p.categoria["nombre"] == "Informatica") numInformatica++;
+            else if(p.categoria["nombre"] == "HogarYCocina") numHogar++;
+            else numLibros++;
+        });
+
+        var maxNum = Math.max(numInformatica, numLibros, numModa, numHogar);
+        var maxCategoria;
+        if(maxNum == numInformatica) maxCategoria = "Informatica";
+        else if(maxNum == numLibros) maxCategoria = "Libros";
+        else if(maxNum == numModa) maxCategoria = "Moda";
+        else maxCategoria = "HogarYCocina";
+        const fileProductos = fs.readFileSync('./productos.json','UTF-8');
+        const datosProductos = JSON.parse(fileProductos);
+        const objetosFiltrados = datosProductos.filter(p => p.categoria["nombre"] == maxCategoria);
+
+        fs.writeFile('./recomendados.json', JSON.stringify(objetosFiltrados), 'utf8', err => {
+            if (err) {
+                console.log("Error al escribir el archivo:", err);
+            }else{
+                console.log("Archivo de resultados creado exitosamente");
+                const recomendados = fs.readFileSync('./recomendados.json','UTF-8');
+                res.send(recomendados);
+            }
+        });
+    } catch (err) {
+        console.log("Error al analizar el archivo JSON:", err);
+    }
+});
+
+app.post('/filtrador', (req, res) =>{
+    let cadena = req.body.filtro;
+    const file = fs.readFileSync('./productos.json','UTF-8');
+    try {
+        const datos = JSON.parse(file);
+        const objetosFiltrados = datos.filter(p => p.categoria["nombre"] == cadena);
+        fs.writeFile('./filtro.json', JSON.stringify(objetosFiltrados), 'utf8', err => {
+            if (err) {
+                console.log("Error al escribir el archivo:", err);
+            }else{
+                console.log("Archivo de resultados creado exitosamente");
+                const filtro = fs.readFileSync('./filtro.json','UTF-8');
+                res.send(filtro);
             }
         });
     } catch (err) {
